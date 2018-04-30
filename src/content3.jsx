eda53358cd9136d42c25738b6f9f32f165a275c7
HonoriaShana <33732069+HonoriaShana@users.noreply.github.com>
2018-04-30 13:59:50 -0400

    Update content3.jsx

--- src/content3.jsx
+++ src/content3.jsx
@@ -11,7 +11,7 @@ export default withStyles({
     fontFamily: 'Ubuntu',
     textAlign: 'center',
     color: "#F5F5F5",
-    fontSize: '1.5rem',
+    fontSize: '1.3rem',
   },
   lbreak: {
     height: '5rem',
