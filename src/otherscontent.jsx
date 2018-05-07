795014d12f2b66942858950882a44399ebff0dfb
HonoriaS1 <33732069+HonoriaS1@users.noreply.github.com>
2018-05-07 13:29:49 -0400

    spicy

--- src/otherscontent.jsx
+++ src/otherscontent.jsx
@@ -53,7 +53,7 @@ export default withStyles({
     <p className={classes.text}>MIT License <br /> <br />
 
 Copyright (c) 2018 Ginkoid
-
+<br />
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
